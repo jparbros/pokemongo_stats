8610ab6f848a4d78af3ca9b242062fc20ab1613f
Jorge Pardiñas <jparbros@gmail.com>
2016-08-08 17:31:01 -0500

    Updates

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sqlite3'
 require 'yaml'
+require 'pry'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
@@ -8,10 +9,14 @@ DB = SQLite3::Database.new "./pogom.db"
 POKEMONS = YAML.load_file('pokemons.yml')
 
 get '/' do
+  total_views = DB.execute("SELECT  COUNT(pokemon_id) FROM pokemon").flatten.first
   @pokemons = DB.execute("SELECT pokemon_id, COUNT(pokemon_id) FROM pokemon GROUP BY pokemon_id ORDER BY pokemon_id").map do |pokemon|
-    {id: pokemon.first, name: POKEMONS[pokemon.first], count: pokemon.last }
+    pokemon_count = pokemon.last
+    chances_to_see = ((pokemon_count.to_f*100)/total_views.to_f).round(2)
+    {id: pokemon.first, name: POKEMONS[pokemon.first], count: pokemon_count, chance: chances_to_see}
   end
   @sorted_pokemons = @pokemons.sort {|a, b| b[:count] <=> a[:count] }
+  @total_counts = @pokemons.inject(0) {|sum, pokemon| sum + pokemon[:count]} 
   erb :index
 end
 
